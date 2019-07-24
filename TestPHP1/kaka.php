76c7a04768567bdee211347994b6ddc148dcfdce
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-07-24 12:07:41 +0700

    PHP S1

---
+++ TestPHP1/kaka.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body style="color: red">
+        <?php
+         echo 'Hello GCH0704';
+        ?>
+    </body>
+</html>
