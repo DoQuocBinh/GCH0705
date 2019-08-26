590a2f583c094164590e94f57b9495f9c336fc82
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-08-26 10:08:53 +0700

    adfas

---
+++ Postgress/show.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<img src="uploads/Photo on 5-17-16 at 8.09 AM.jpg" width="200px" height="200px">
+</body>
+</html>
